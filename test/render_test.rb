da07ba3ed19a93ce2f1069c8b5f1ba2f9636f1bf
sklemm <sebi@phoenix.(none)>
2009-05-23 11:56:00 +0200

    prepare for svg: dried render method

--- test/render_test.rb
+++ test/render_test.rb
@@ -9,10 +9,12 @@ class RenderTest < Test::Unit::TestCase
   include RCDK::Util
   include RCDK::Render
 
-  def test_write_png()
+  def test_render_imgs()
     molecule = Lang.read_smiles('Clc1ccccc1')
-    Painter.write_png(molecule, 'output/new_chlorobenzene.png', 100, 100)
+    Painter.write_png(molecule, 'output/new_chlorobenzene.png', 200, 200)
+    Painter.write_jpg(molecule, 'output/new_chlorobenzene.jpg', 200, 200)
     molecule = Lang.read_smiles('CC(=O)OC1=CC=CC=C1C(=O)O')
-    Painter.write_png(molecule, 'output/new_aspirin.png', 100, 100)
+    Painter.write_png(molecule, 'output/new_aspirin.png', 200, 200)
+    Painter.write_jpg(molecule, 'output/new_aspirin.jpg', 200, 200)
   end
 end
